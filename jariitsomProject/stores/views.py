2cf89cbe97cf9499d2417a55c20c214b29c47b87
kososb <koso100145@gmail.com>
2025-08-10 02:25:49 +0900

    병합 완료

--- jariitsomProject/stores/views.py
+++ jariitsomProject/stores/views.py
@@ -1,5 +1,5 @@
 import json
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
